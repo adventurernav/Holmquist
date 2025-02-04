a3a6745724104a8195938ad129b419f4ad0c43a9
adventurernav <adventurernav@gmail.com>
2025-02-04 05:47:26 -0500

    add remove from cart button

--- client/script.js
+++ client/script.js
@@ -3,15 +3,20 @@ const cartElement = document.getElementById("cart");
 let products = [];
 let cart = [];
 
+let emptyCart = () => {
+  cart = [];
+  cartElement.replaceChildren();
+};
+
 function openModal(id) {
   document.getElementById(id).classList.add("open");
   document.body.classList.add("modal-open");
 }
 
-// close currently open modal
-function closeModal() {
+function cancelModal() {
   document.querySelector(".modal.open").classList.remove("open");
   document.body.classList.remove("modal-open");
+  emptyCart();
 }
 
 window.addEventListener("load", function () {
@@ -24,11 +29,40 @@ window.addEventListener("load", function () {
 });
 
 function addItemToOrder(event) {
+  let newLi = document.createElement("li");
+  let itemDeleteButton = document.createElement("button");
   const thisItem = event.target.productInfo;
-  let newCartItem = document.createElement("li")
-  newCartItem.innerText=thisItem;
-cartElement.appendChild(newCartItem)
+  itemDeleteButton.textContent = "Delete";
+  itemDeleteButton.productInfo = thisItem;
+  itemDeleteButton.addEventListener("click", removeItemFromOrder);
+  newLi.innerText = thisItem;
+  newLi.appendChild(itemDeleteButton);
+  cartElement.appendChild(newLi);
+  cart.push(thisItem);
 }
+
+let removeItemFromOrder = (event) => {
+  let productInfo = event.target.productInfo;
+  let indexToRemove = -1;
+
+  cart.forEach((cartItem) => {
+
+    if (productInfo[0] === cartItem[0] && productInfo[1] === cartItem[1]) {
+      indexToRemove = cart.indexOf(cartItem);
+      if (indexToRemove>=0) {
+        cart.splice(indexToRemove,1)
+      }
+    }
+
+  });
+console.log(cart);
+};
+
+let submitCart = () => {
+  // Send the current cart info to the DB and complete the sale
+  console.log(cart);
+  emptyCart();
+};
 
 fetch("data.json", {
   method: "GET",
