003b9278981ffd59f27b94869efcfa2bb5e35e61
adventurernav <adventurernav@gmail.com>
2025-02-05 02:04:02 -0500

    import mui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import SimpleBottomNavigation from './Functions';
 
 function App() {
+  // use mui "stepper" for each sale.
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav></nav>
       </header>
+
+
+
+<footer><SimpleBottomNavigation /></footer>
     </div>
   );
 }
---
+++ src/components/Sales.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Sales extends Component {
+  render() {
+    return (
+      <div><h1>Let's sell some shit</h1></div>
+    )
+  }
+}
+
+export default Sales
