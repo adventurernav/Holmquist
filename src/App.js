fe922f51098c16b382e638ef12056510f8f7176d
adventurernav <adventurernav@gmail.com>
2025-02-05 02:04:51 -0500

    remove junk

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import './App.css';
-import SimpleBottomNavigation from './Functions';
+import "./App.css";
+import SimpleBottomNavigation from "./Functions";
 
 function App() {
   // use mui "stepper" for each sale.
   return (
     <div className="App">
-      <header className="App-header">
-        <nav></nav>
-      </header>
 
-
-
-<footer><SimpleBottomNavigation /></footer>
+      <footer>
+        <SimpleBottomNavigation />
+      </footer>
     </div>
   );
 }
